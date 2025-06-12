bf61eb376757d326508b3a31adccf1d25264fa2b
milaaulia21 <milaauliap597@gmail.com>
2025-06-13 00:51:33 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\DosenController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\DosenController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Di sini didefinisikan semua route frontend Laravel yang terhubung ke
+| backend CodeIgniter 4 (API). Tidak ada middleware auth.
+|
+*/
 
-// Mahasiswa
-Route::get('/mahasiswa/input', function () {
-    return view('mahasiswa.input');
-});
+// Dashboard (halaman utama)
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+// CRUD Mahasiswa
 Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
-Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+Route::get('/mahasiswa/create', [MahasiswaController::class, 'create'])->name('mahasiswa.create');
 Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
+Route::get('/mahasiswa/{id}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
 Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
-Route::get('/mahasiswa/edit/{id}', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
 
-// Dosen
-Route::get('/dosen/input', [DosenController::class, 'create'])->name('dosen.create'); // ubah ke controller
-Route::get('/dosen/index', [DosenController::class, 'index'])->name('dosen.index'); // ubah ke controller
+// CRUD Dosen
+Route::get('/dosen', [DosenController::class, 'index'])->name('dosen.index');
+Route::get('/dosen/create', [DosenController::class, 'create'])->name('dosen.create');
 Route::post('/dosen', [DosenController::class, 'store'])->name('dosen.store');
+Route::get('/dosen/{id}/edit', [DosenController::class, 'edit'])->name('dosen.edit');
 Route::put('/dosen/{id}', [DosenController::class, 'update'])->name('dosen.update');
 Route::delete('/dosen/{id}', [DosenController::class, 'destroy'])->name('dosen.destroy');
-Route::get('/dosen/edit/{id}', [DosenController::class, 'edit'])->name('dosen.edit');
